e5f417149f97515b3a16b6257359e2da713abb8b
foundersventlite-sys <foundersventlite@gmail.com>
2026-08-22 16:30:57 +0600

    Create permissions.js

---
+++ src/config/permissions.js
@@ -0,0 +1,16 @@
+export const PERMISSIONS = {
+  ADMIN: [
+    "manage_clients",
+    "manage_landing_pages",
+    "manage_templates",
+    "manage_orders",
+    "manage_requests",
+    "manage_settings",
+  ],
+  CLIENT: [
+    "view_dashboard",
+    "view_landing_page",
+    "create_order",
+    "submit_edit_request",
+  ],
+};
