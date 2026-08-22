c88d8f6df2de82bdbac7e1c48ac95ffb4858ad11
foundersventlite-sys <foundersventlite@gmail.com>
2026-08-22 21:16:22 +0600

    Create storageService.js

---
+++ src/services/storageService.js
@@ -0,0 +1,6 @@
+export const storageService = {
+  get: (key) => localStorage.getItem(key),
+  set: (key, value) => localStorage.setItem(key, value),
+  remove: (key) => localStorage.removeItem(key),
+  clear: () => localStorage.clear(),
+};
