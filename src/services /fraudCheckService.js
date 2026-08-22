745b4bcf957cba80f3b1217c3763013ce0e88cb9
foundersventlite-sys <foundersventlite@gmail.com>
2026-08-22 21:19:28 +0600

    Delete src/services /fraudCheckService.js

--- src/services /fraudCheckService.js
+++
@@ -1,9 +0,0 @@
-import { apiRequest } from "./apiClient";
-
-export const fraudCheckService = {
-  check: (phone) =>
-    apiRequest("/fraud-check", {
-      method: "POST",
-      body: JSON.stringify({ phone }),
-    }),
-};
