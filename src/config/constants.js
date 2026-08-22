c28586b8a00e83d78d71788ce32a2c5531107fb8
foundersventlite-sys <foundersventlite@gmail.com>
2026-08-22 16:29:41 +0600

    Create constants.js

---
+++ src/config/constants.js
@@ -0,0 +1,27 @@
+export const APP_NAME = "LandingPro Core";
+
+export const USER_ROLES = {
+  ADMIN: "admin",
+  CLIENT: "client",
+};
+
+export const PAGE_STATUS = {
+  DRAFT: "Draft",
+  PUBLISHED: "Published",
+};
+
+export const ORDER_STATUS = {
+  PENDING: "Pending",
+  CONFIRMED: "Confirmed",
+  CANCELLED: "Cancelled",
+  DELIVERED: "Delivered",
+  RETURNED: "Returned",
+};
+
+export const TEMPLATES = [
+  "Template 1",
+  "Template 2",
+  "Template 3",
+  "Template 4",
+  "Template 5",
+];
