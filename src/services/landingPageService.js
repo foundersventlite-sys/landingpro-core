6fc9e3249f6ff2eb2d714c0063c83cf12e4348bc
foundersventlite-sys <foundersventlite@gmail.com>
2026-08-23 12:36:39 +0600

    Update landingPageService.js

--- src/services/landingPageService.js
+++ src/services/landingPageService.js
@@ -1,11 +1,40 @@
 import { apiRequest } from "./apiClient";
 
 export const landingPageService = {
-  getAll: () => apiRequest("/landing-pages"),
-  getById: (id) => apiRequest(`/landing-pages/${id}`),
-  create: (data) => apiRequest("/landing-pages", { method: "POST", body: JSON.stringify(data) }),
-  update: (id, data) => apiRequest(`/landing-pages/${id}`, { method: "PUT", body: JSON.stringify(data) }),
-  publish: (id) => apiRequest(`/landing-pages/${id}/publish`, { method: "POST" }),
-  unpublish: (id) => apiRequest(`/landing-pages/${id}/unpublish`, { method: "POST" }),
-  remove: (id) => apiRequest(`/landing-pages/${id}`, { method: "DELETE" }),
+  getAll: () => apiRequest("/landing-pages/list"),
+
+  getById: (id) =>
+    apiRequest(`/landing-pages/get?id=${encodeURIComponent(id)}`),
+
+  create: (data) =>
+    apiRequest("/landing-pages/create", {
+      method: "POST",
+      body: JSON.stringify(data),
+    }),
+
+  update: (id, data) =>
+    apiRequest("/landing-pages/update", {
+      method: "PUT",
+      body: JSON.stringify({
+        id,
+        ...data,
+      }),
+    }),
+
+  publish: (id) =>
+    apiRequest("/landing-pages/publish", {
+      method: "POST",
+      body: JSON.stringify({ id }),
+    }),
+
+  unpublish: (id) =>
+    apiRequest("/landing-pages/unpublish", {
+      method: "POST",
+      body: JSON.stringify({ id }),
+    }),
+
+  remove: (id) =>
+    apiRequest(`/landing-pages/delete?id=${encodeURIComponent(id)}`, {
+      method: "DELETE",
+    }),
 };
